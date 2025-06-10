529899d91076461899efe61c31fc118e20ab47b6
Minjae Cho <whalswo117@gmail.com>
2025-06-10 09:58:25 -0500

    asd

--- utils/get_args.py
+++ utils/get_args.py
@@ -57,7 +57,7 @@ def get_args():
     parser.add_argument("--algo-name", type=str, default="ppo", help="Disable cuda.")
     parser.add_argument("--seed", type=int, default=42, help="Batch size.")
     parser.add_argument(
-        "--num-runs", type=int, default=5, help="Number of samples for training."
+        "--num-runs", type=int, default=10, help="Number of samples for training."
     )
     parser.add_argument(
         "--num-options", type=int, default=None, help="Number of samples for training."
